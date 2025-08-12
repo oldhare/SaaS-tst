b82a51a95eb7434683c8a4afd1644efe9d5d5907
oldhare <mihal18@yandex.ru>
2025-08-12 12:51:30 +0300

    Second commit

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -21,10 +21,16 @@ def home_view(request, *args, **kwargs):
 def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
-    try:
-        percent = (page_qs.count() * 100.0) / qs.count()
-    except:
-        percent = 0
+    # try:
+    #     percent = (page_qs.count() * 100.0) / qs.count()
+    # except:
+    #     percent = 0
+    total_count = qs.count()
+if total_count > 0:
+    percent = (page_qs.count() * 100.0) / total_count
+else:
+    percent = 0
+
     my_title = "My Page"
     html_template = "home.html"
     my_context = {
